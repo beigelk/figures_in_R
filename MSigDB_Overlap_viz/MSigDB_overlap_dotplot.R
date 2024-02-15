6593cf007518a53fc25c593e1b48b2fd6f338ac0
Katherine Beigel <123108611+beigelk@users.noreply.github.com>
2024-02-15 13:22:50 -0500

    Update MSigDB_overlap_dotplot.R

--- MSigDB_Overlap_viz/MSigDB_overlap_dotplot.R
+++ MSigDB_Overlap_viz/MSigDB_overlap_dotplot.R
@@ -1,7 +1,7 @@
-gsea_genes_all = read_tsv("GSEA_overlap_table.tsv") %>% mutate(`Gene Set Name` = fct_reorder(`Gene Set Name`, `k/K`))
+gsea_genes_all = read_tsv("MSigDB_overlap_table.tsv") %>% mutate(`Gene Set Name` = fct_reorder(`Gene Set Name`, `k/K`))
 
 ggplot(gsea_genes_all, aes(x = `k/K`, y = `Gene Set Name`, color = -log10(`FDR q-value`), size = `# Genes in Overlap (k)`)) +
   geom_point(stat = 'identity') + 
-  xlab("Gene ratio (# Genes in Overlap / # Genes in Gene Set") + ylab("Pathway") + ggtitle("GSEA") + 
+  xlab("Gene ratio (# Genes in Overlap / # Genes in Gene Set") + ylab("Pathway") + ggtitle("") + 
   theme_bw() +
   scale_color_gradient(low = "blue", high = "red")
